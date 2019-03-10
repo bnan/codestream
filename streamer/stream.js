2cbd4f08afd74e6bb21ff28dce454ed77d339938
Fábio Maia <fabiu.maia@gmail.com>
2019-03-10 04:26:53 +0000

    Refactor args

--- streamer/stream.js
+++ streamer/stream.js
@@ -1,20 +1,31 @@
 const chokidar = require('chokidar')
 const fs = require('fs')
 const WebSocket = require('ws')
+const ArgumentParser = require('argparse').ArgumentParser;
 const utils = require('./utils')
-const argv = process.argv.slice(2)
-const ws = new WebSocket(argv[1])
 
-function stream(path) {
+function error() {
+  utils.error(`Server is not reachable.`)
+  process.exit(1)
+}
+
+function stream(ws, path) {
   if (ws.readyState === WebSocket.OPEN) {
-    utils.info(`File ${path} changed on disk, streaming to central server...`)
+    utils.info(`File ${path} changed on disk, streaming to server...`)
     fs.readFile(path, 'utf8', (err, contents) => {
       ws.send(contents)
     })
   } else {
-    utils.error(`Central server is not reachable.`)
-    process.exit(1)
+    error()
   }
 }
 
-chokidar.watch(argv[0]).on('change', (path) => stream(path))
+
+const parser = new ArgumentParser({ version: '0.0.1', addHelp: true, description: 'Stream a file to viewers' })
+parser.addArgument( [ '-f', '--file' ], { required: true, help: 'filename' })
+parser.addArgument( [ '-u', '--url' ], { required: true, help: 'central server URL' })
+const args = parser.parseArgs()
+
+const ws = new WebSocket(args.url)
+ws.on('error', () => error())
+chokidar.watch(args.file).on('change', (path) => stream(ws, path))
